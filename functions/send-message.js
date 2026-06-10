107672ea5383812a2cc31ad5dd337f0dbbacbaa6
msinamo <mohammadsinamotlagh@gmail.com>
2026-06-10 21:15:06 +0330

    Update send-message.js

--- functions/send-message.js
+++ functions/send-message.js
@@ -1,1 +1,16 @@
+export async function onRequestPost(context) {
+  const db = context.env.DB;
 
+  const { userId, sender, message } =
+    await context.request.json();
+
+  await db.prepare(
+    "INSERT INTO messages (user_id,sender,message) VALUES (?,?,?)"
+  )
+  .bind(userId, sender, message)
+  .run();
+
+  return Response.json({
+    success: true
+  });
+}
