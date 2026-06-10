d2d033570a803b5ddbdaf80e4b383d76be1fa80b
msinamo <mohammadsinamotlagh@gmail.com>
2026-06-10 21:15:43 +0330

    Update get-messages.js

--- functions/get-messages.js
+++ functions/get-messages.js
@@ -1,1 +1,16 @@
+export async function onRequest(context) {
 
+  const db = context.env.DB;
+
+  const userId = new URL(
+    context.request.url
+  ).searchParams.get("userId");
+
+  const result = await db.prepare(
+    "SELECT * FROM messages WHERE user_id=? ORDER BY id ASC"
+  )
+  .bind(userId)
+  .all();
+
+  return Response.json(result.results);
+}
