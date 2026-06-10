9427b93efe6f972e8a5a0a36341f247fdef7021a
msinamo <mohammadsinamotlagh@gmail.com>
2026-06-10 21:14:28 +0330

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -1,1 +1,23 @@
+export async function onRequestPost(context) {
+  const db = context.env.DB;
 
+  const { username, password } =
+    await context.request.json();
+
+  const user = await db.prepare(
+    "SELECT * FROM users WHERE username=? AND password=?"
+  )
+  .bind(username, password)
+  .first();
+
+  if (!user) {
+    return Response.json({
+      success: false
+    });
+  }
+
+  return Response.json({
+    success: true,
+    user
+  });
+}
