a16dac4213fe0c1f243dd0098a2c05e30337a3e5
msinamo <mohammadsinamotlagh@gmail.com>
2026-06-10 21:13:59 +0330

    Update register.js

--- functions/register.js
+++ functions/register.js
@@ -1,1 +1,22 @@
+export async function onRequestPost(context) {
+  const db = context.env.DB;
+  const { username, password } = await context.request.json();
 
+  try {
+    await db.prepare(
+      "INSERT INTO users (username, password) VALUES (?, ?)"
+    )
+    .bind(username, password)
+    .run();
+
+    return Response.json({
+      success: true
+    });
+
+  } catch {
+    return Response.json({
+      success: false,
+      error: "username exists"
+    });
+  }
+}
